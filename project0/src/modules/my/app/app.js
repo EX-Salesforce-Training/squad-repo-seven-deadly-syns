1413fe48bc78c048ffa66221f35994ead73a09d6
MoAdam10 <meedoadam@gmail.com>
2021-11-17 22:53:05 -0500

    added logo and css clean up

--- project0/src/modules/my/app/app.js
+++ project0/src/modules/my/app/app.js
@@ -8,4 +8,18 @@ export default class App extends LightningElement {
     underarmourId = 3;
     newbalanceId = 4;
     @api myData = data;
+    @api input = '';
+
+    about =
+        'We Are ProCleats and we have the best professional sports clearts in the market';
+
+    handleSubmit(event) {
+        this.input = event.target.value;
+        let filtered = input.toLowerCase();
+        return this.myData.filter((product) => {
+            if (product) {
+                product.adidas.filter((item) => item.name !== filtered);
+            }
+        });
+    }
 }
