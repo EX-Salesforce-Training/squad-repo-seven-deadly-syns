ff19adfe2fa4dc2119520119ef3ea018875bdc3c
Christopher Brennan <Christopher.Brennan@revature.net>
2021-11-17 19:56:00 -0500

    Made it look better an added hover functionality. JS is still a mess because LWC doesn't like working

--- Project_0/interaction.js
+++ Project_0/interaction.js
@@ -1,13 +1,24 @@
 import { LightningElement } from "lwc";
-import { NavigationMixin } from 'lightning/navigation';
 
-export default class MainWebpage extends NavigationMixin(LightningElement) 
+export default class MainWebpage extends LightningElement
 {
+
+  //imagesTest = "https://numerograph.files.wordpress.com/2020/02/numerology-number-1.jpg";
+  image1 = "https://numerograph.files.wordpress.com/2020/02/numerology-number-1.jpg";
+  image2 = "https://numerograph.files.wordpress.com/2020/02/numerology-number-2.jpg?w=600";
+  image3 = "https://cdn.pixabay.com/photo/2012/04/23/17/07/three-39116_1280.png";
+  image4 = "https://numerograph.files.wordpress.com/2020/02/numerology-number-4-lucky-colors.jpg?w=600";
+  image5 = "https://media.istockphoto.com/photos/3d-gold-white-number-5-five-isolated-white-background-picture-id518070002";
 
 
   connectedCallback()
   {
-
+    /*
+    for(let element of document.getElementById("testimg"))
+    {
+      console.log(this.imagesTest);
+      element.setAttribute.src = this.imagesTest;
+    }
+    */
   }
-
 }
